0c562b621742b5d54448d0253c2127a572cb8f8e
marlonschulze <marlonschulze2804@gmail.com>
2025-07-16 13:41:51 -0300

    Caminho JS/atalhos

--- js/atalhos.js
+++ js/atalhos.js
@@ -2,22 +2,22 @@ document.addEventListener('keydown', (e) => {
   switch(e.key){
     case 'F1':
       e.preventDefault();
-      window.location.href = '/home/home.html';
+      window.location.href = '../home/home.html';
       break;
 
     case 'F2':
       e.preventDefault();
-      window.location.href = '/sales/sales.html';
+      window.location.href = '../sales/sales.html';
       break;
 
     case 'F3':
       e.preventDefault();
-      window.location.href = '/stock/stock.html';
+      window.location.href = '../stock/stock.html';
       break;
 
     case 'F4':
       e.preventDefault();
-      window.location.href = '/report/report.html';
+      window.location.href = '../report/report.html';
       break;
   };
 })
